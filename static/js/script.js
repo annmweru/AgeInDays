471ded87766eef023f27f22c7967b2c048689dfe
annmweru <annmweru9@gmail.com>
2020-04-14 14:18:02 -0700

    cat generator using html,css,js

--- static/js/script.js
+++ static/js/script.js
@@ -1,13 +1,10 @@
 
    
-  function ageInDays () {
-    var birthYear =prompt('what year were you born?');
-     var ageInDayss=(2020 - birthYear) * 365;
-var h1=document.createElement('h1');
-   var textAnswer=document.createTextNode('you are' + ageInDayss +'days old.');
-   h1.setAttribute('id','ageInDays');
-  h1.appendChild(textAnswer);
-  document.getElementById('flex-box.result').appendChild(hi);
-  }
-
-
+ 
+//challenge two;
+function generateCat(){
+var image=document.createElement('img');
+var div=document.getElementById('flex-cat-gen')
+image.src="http://thecatapi.com/api/images/get?format=src&type=gif&size=small";
+div.appendChild(image); 
+}
